830adea3d269d6c11f7107ca31d213800b6b6235
Nan Jiang <jn1473733091@163.com>
2025-01-25 03:01:04 +0800

    新增员工代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
 public interface EmployeeService {
+
+    void add(EmployeeDTO employeeDTO);
 
     /**
      * 员工登录
